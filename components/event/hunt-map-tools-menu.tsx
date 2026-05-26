486cdffd9950ddc3e72d62d41a2b01d127c6ea56
Karl Jansson <karljansson91@gmail.com>
2026-05-26 12:59:20 +0200

    Use map icon for hunt map tools

--- components/event/hunt-map-tools-menu.tsx
+++ components/event/hunt-map-tools-menu.tsx
@@ -181,7 +181,7 @@ export function HuntMapToolsMenu({
           tone="dark"
           className="size-14 rounded-full"
           contentClassName="h-full w-full items-center justify-center">
-          <Ionicons name="locate" size={24} color={APP_COLORS.surface} />
+          <Ionicons name="map-outline" size={24} color={APP_COLORS.surface} />
         </GlassSurface>
       </View>
     </MenuView>
